ea91d5807496dc234bc50d30289477127b49588f
toto <homeboyzhou@foxmail.com>
2019-08-31 20:04:37 +0800

    first commit

---
+++ src/main/java/com/github/ciifm/config/service/ConfigApplication.java
@@ -0,0 +1,15 @@
+package com.github.ciifm.config.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
